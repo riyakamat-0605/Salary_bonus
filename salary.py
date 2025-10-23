e24dfbea92310a960d2f24db7e6ffd27a6e25171
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-23 15:47:38 +0530

    save to tax-deduction

--- salary.py
+++ salary.py
@@ -2,8 +2,9 @@ salary = float(input("Enter employee salary: ₹"))
 
 bonus = salary * 0.10
 salary_with_bonus = salary + bonus
+tax = salary_with_bonus * 0.05
+final_salary = salary_with_bonus - tax
 
 print(f"Original Salary: ₹{salary}")
 print(f"Bonus Added: ₹{salary_with_bonus}")
-
-
+print(f"After Tax Deduction: ₹{final_salary}")
