80c9c28a78633ef1633f106d7d44f4989635b993
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-23 15:46:00 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -1,2 +1,9 @@
 salary = float(input("Enter employee salary: ₹"))
+
+bonus = salary * 0.10
+salary_with_bonus = salary + bonus
+
 print(f"Original Salary: ₹{salary}")
+print(f"Bonus Added: ₹{salary_with_bonus}")
+
+
